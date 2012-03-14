0c9100057776d409093964a69b1b8c58f1f1cc92
James Saryerwinnie <jls.npi@gmail.com>
2012-03-14 12:05:33 -0700

    Bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ from setuptools import setup
 
 setup(
     name='Flask-Shelve',
-    version='0.1',
+    version='0.1.1',
     url='https://github.com/jamesls/flask-shelve',
     license='BSD',
     author='James Saryerwinnie',
